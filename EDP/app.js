79739eaa0e46fe111132032eab57206bbf3d1272
FetuStudio <fetu.studio@encantia.lat>
2025-02-22 12:02:19 +0100

    Update app.js

--- EDP/app.js
+++ EDP/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-    var map = L.map('map').setView([20, 0], 2); 
+    // Inicializar el mapa de Leaflet
+    var map = L.map('map').setView([20, 0], 2); // Vista centrada en el mundo
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
+    // Función para obtener resultados de la propagación DNS
     function fetchDNSPropagation(domain) {
         $.ajax({
             url: 'dnsQuery.php',
@@ -13,33 +15,28 @@ $(document).ready(function () {
             success: function(response) {
                 var data = JSON.parse(response);
                 showResults(data);
-                updateMap(data);
             },
             error: function() {
-                alert("Hubo un error al consultar la propagación DNS.");
+                alert("Hubo un error al consultar los registros DNS.");
             }
         });
     }
 
+    // Mostrar los resultados de la consulta CNAME
     function showResults(data) {
         $('#results').show();
         $('#resultList').empty();
         
-        data.forEach(function(result) {
-            $('#resultList').append('<li>' + result.location + ': ' + result.status + '</li>');
-        });
+        if (data.status) {
+            $('#resultList').append('<li>' + data.status + '</li>');
+        } else {
+            data.forEach(function(result) {
+                $('#resultList').append('<li>' + result.domain + ' tiene un CNAME que apunta a: ' + result.cname + '</li>');
+            });
+        }
     }
 
-    function updateMap(data) {
-        data.forEach(function(result) {
-            var statusColor = result.status === 'OK' ? 'green' : 'red';
-            L.circleMarker([result.latitude, result.longitude], {
-                color: statusColor,
-                radius: 10
-            }).addTo(map);
-        });
-    }
-
+    // Enviar formulario
     $('#dnsForm').submit(function (event) {
         event.preventDefault();
         var domain = $('#domain').val();
